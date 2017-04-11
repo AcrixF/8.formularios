4e176f5b66d98a3aa550b1fb4d2e6d9432a3a5f6
AcrixF <acrixf@gmail.com>
2017-04-11 17:25:35 -0500

    enfoque template

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: [`.ng-invalid.ng-touched:not(form){
+    border: 1px solid red;
+  }`]
+})
+export class TemplateComponent implements OnInit {
+
+  usuario: Object = {
+    nombre: null,
+    apellido: null,
+    correo: null,
+    pais : "",
+    sexo: "",
+    aceptar: false
+  }
+
+  paises = [
+    {
+      codigo:"CRI",
+      nombre: "Costa Rica"
+    },
+    {
+      codigo:"ESP",
+      nombre:"España"
+    }
+    ];
+
+
+  sexos: string[] = ["Hombre","Mujer", "Sin definir"];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  guardar(forma:any){
+    console.log("Posteado")
+    console.log("NgForm", forma);
+    console.log("valor", forma.value)
+  }
+
+}
